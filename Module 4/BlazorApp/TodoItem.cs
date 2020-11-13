68b290ea7ba0b3a419fcd78db2ebcd0df625bbee
AdamShandi <adam.cheleby@gmail.com>
2020-11-13 00:22:20 -0800

    Submission for Module 4 – Adam-Shandi

---
+++ Module 4/BlazorApp/TodoItem.cs
@@ -0,0 +1,12 @@
+// public class TodoItem
+// {
+
+//     public string Title { get; set; }
+//     public bool isDone { get; set; }
+    
+// }
+
+// - The progeam is making a simple TodoList
+// - The program has an HTML field and a button
+// - A button connected to onclick event listener to add the todo item
+// - we push the item to the list of Todos object has a title file 
